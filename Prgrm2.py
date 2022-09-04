2901118b5593b4510ba781e1e0ad7cc46017fa3f
Hider13 <109530580+Hider13@users.noreply.github.com>
2022-09-04 17:04:15 +0530

    Delete Prgrm2.py

--- Prgrm2.py
+++
@@ -1,9 +0,0 @@
-import os
-file_name=input("ENter file name : ")
-if(os.path.exists(file_name)):
-    print("File exsits..The contents in ascending order are..")
-    with open(file_name,'r') as f:
-        print(sorted(f.readlines()))
-else:
-    print("file does not exist")
-    
