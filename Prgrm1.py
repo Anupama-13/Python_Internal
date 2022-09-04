941a64570e9df28e85791e78123c0b6947ca14b1
Hider13 <109530580+Hider13@users.noreply.github.com>
2022-09-04 16:53:52 +0530

    Delete Prgrm1.py

--- Prgrm1.py
+++
@@ -1,5 +0,0 @@
-details=[['Anu',21],['Divya',40],['chandu',50],['Ak',19]]
-print("The list of names nad ages before sorting is")
-print(details)
-print("The list after sorting based on age is")
-print(sorted(details,key=lambda x:x[-1]))
